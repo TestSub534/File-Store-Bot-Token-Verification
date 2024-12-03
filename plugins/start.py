32258b1b357c30412004f9f2c6890f0d37d9d675
TestSub534 <nsa837967@gmail.com>
2024-12-03 10:39:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -139,7 +139,7 @@ async def start_command(client: Client, message: Message):
         elif verify_status['is_verified']:
             reply_markup = InlineKeyboardMarkup(
                 [[InlineKeyboardButton("⚡️ About", callback_data = "about"),
-                  InlineKeyboardButton('🍁 Dark Files', url='https://t.me/swbackups')]]
+                  InlineKeyboardButton('🍁 Backups', url='https://t.me/swbackups')]]
             )
             await message.reply_text(
                 text=START_MSG.format(
@@ -163,8 +163,8 @@ async def start_command(client: Client, message: Message):
                 await update_verify_status(id, verify_token=token, link="")
                 link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
                 btn = [
-                    [InlineKeyboardButton("• ᴏᴘᴇɴ ʟɪɴᴋ", url=link)],
-                    [InlineKeyboardButton('ᴛᴜᴛᴏʀɪᴀʟ •', url=TUT_VID)]
+                    [InlineKeyboardButton("ᴏᴘᴇɴ ʟɪɴᴋ", url=link)],
+                    [InlineKeyboardButton('ᴛᴜᴛᴏʀɪᴀʟ', url=TUT_VID)]
                 ]
                 await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 Hour after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
 
